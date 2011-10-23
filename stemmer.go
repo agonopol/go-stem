c3f6ecf67a33e4f0c40fe1ba17fd41260f98b3a8
Alex Gonopolskiy <agonopol@gmail.com>
2011-10-23 12:49:27 -0500

    shie

--- stemmer.go
+++ stemmer.go
@@ -5,7 +5,7 @@ func consonant(body []byte, offset int) bool {
 	case 'A', 'E', 'I', 'O', 'U':
 		return false
 	case 'Y':
-		if len(body) + 1 < offset {
+		if len(body)+1 < offset {
 			return !consonant(body, offset+1)
 		}
 		return false
@@ -18,24 +18,23 @@ func vowel(body []byte, offset int) bool {
 }
 
 const (
-    vowel_state = iota
-	consonant_state 
-	)
-
+	vowel_state = iota
+	consonant_state
+)
 
 func meansure(body []byte) int {
 	meansure := 0
 	var state int
-	if vowel(body,0) {
+	if vowel(body, 0) {
 		state = vowel_state
 	} else {
 		state = consonant_state
 	}
-	for i := 0;i<len(body);i++{
+	for i := 0; i < len(body); i++ {
 		if vowel(body, i) && state == consonant_state {
 			state = vowel_state
 			meansure++
-		} else if consonant(body,i) && state == vowel_state {
+		} else if consonant(body, i) && state == vowel_state {
 			state = consonant_state
 			meansure++
 		}
