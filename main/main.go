97b767c9062729ec175b2968a89039d4f4cee205
Jeremy Wall <jwall@jwall-macbookpro.wall.fam>
2012-04-05 20:46:26 -0500

    Update to Go 1 and make package go gettable.
    * move the library files into a stemmer directory so the trailing directory
      matches the package name.
    ** Move the test data files into the stemmer directory.
    * Move the main test file into a main directory.
    * Remove the Makefile it's no longer necessary.

--- main/main.go
+++ main/main.go
@@ -1,14 +1,16 @@
 package main
 
-import "stemmer"
-import "bufio"
-import "os"
+import (
+	"github.com/zaphar/go-stem/stemmer"
+	"bufio"
+	"os"
+)
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
-	
+
 	for word, err := in.ReadSlice('\n'); err == nil; word, err = in.ReadSlice('\n') {
 		out.Write(stemmer.Stem(word))
 		out.WriteString("\n")
